fe59a270fd8a0bcf2a3271c4b691afd9e6c3f2e4
vanduoc <vanduoc198@gmail.com>
2022-08-19 16:01:44 +0700

    Cấu hình Router/Layout

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import { HeaderOnly } from '~/components/Layout';
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+// do not login but can see
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+// must login to see
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
